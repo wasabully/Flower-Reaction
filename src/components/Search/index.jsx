e6ea6bff827bf68d4175d70718a1bb403e89593a
Danil <dd.wasabully@gmail.com>
2025-02-14 18:30:36 +0300

    feat: add input focus management with useRef

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -4,6 +4,12 @@ import styles from './Search.module.scss';
 
 function Search() {
 	const { searchValue, setsearchValue } = React.useContext(SearchContext);
+	const inputRef = React.useRef();
+
+	const onClickClear = () => {
+		setsearchValue('');
+		inputRef.current.focus();
+	};
 	return (
 		<div className={styles.root}>
 			<svg
@@ -18,6 +24,7 @@ function Search() {
 				<path d='M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z'></path>
 			</svg>
 			<input
+				ref={inputRef}
 				value={searchValue}
 				onChange={(event) => setsearchValue(event.target.value)}
 				placeholder='Поиск букетов'
@@ -25,7 +32,7 @@ function Search() {
 			/>
 			{searchValue && (
 				<svg
-					onClick={() => setsearchValue('')}
+					onClick={onClickClear}
 					className={styles.clear}
 					data-name='Capa 1'
 					id='Capa_1'
